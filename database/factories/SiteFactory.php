e5c9ba160ecdbb8fd5f99646399ccb1ccd3a7a7b
Cameron <cammbilly8@gmail.com>
2023-01-03 20:17:57 -0500

    Add site factory

---
+++ database/factories/SiteFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Site>
+ */
+class SiteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->sentence(),
+            'tags' => 'green, cold, rad',
+            'location' => fake()->word(),
+            'directions' => fake()->sentence(),
+            'description' => fake()->paragraph(3)
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,21 +16,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          \App\Models\User::factory(10)->create();
-
-        Site::create([
-            'title' => 'Site 14!',
-            'tags' => 'southern, nearby water',
-            'location' => 'Pinhoti Trail, Alabama',
-            'description' => 'This is a grand ole campsite with lots of bears and squirrels',
-            'directions' => 'Its a secret!'
-        ]);
-
-        Site::create([
-            'title' => 'Sweet Home Nebraska',
-            'tags' => 'dusty, spooky',
-            'location' => 'Lincoln, Nebraska',
-            'description' => 'Not too much to see but it works',
-            'directions' => "It\'s on the side of the interstate"
-        ]);
+         Site::factory(10)->create();
     }
 }
